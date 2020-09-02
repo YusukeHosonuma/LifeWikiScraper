8c4985cf21fb472e3ed084e4e8a35589130f56d5
Yusuke Hosonuma <tobi462@gmail.com>
2020-09-02 20:55:20 +0900

    fix: result stream

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -2,7 +2,7 @@ import Foundation
 import Scraper
 import Combine
 
-let fetchCount = 200 // 最後には全部取得するので不要になる
+let fetchCount = 20 // 最後には全部取得するので不要になる
 
 // TODO: 現状よくわかっていないが、`asyncAfter`で遅延実行しないと`RunLoop`の処理に到達しない。ので暫定対処
 
@@ -11,37 +11,38 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
     _ = LifeWikiAllPatternPage.fetchAll()
         .flatMap { (pages: [LifeWikiAllPatternPage]) -> AnyPublisher<[LifeWikiPatternPage], Never> in
             print("⚡️ Start fetch Pattern pages.")
+            let initial = Just([LifeWikiPatternPage]()).eraseToAnyPublisher()
             return pages.map(\.links).joined()
                 .prefix(fetchCount)
-                .reduce(
-                    Just([LifeWikiPatternPage]()).eraseToAnyPublisher()
-                ) { (result, link) in
+                .reduce(initial) { (result, link) in
                     result.zip(LifeWikiPatternPage.fetch(url: link))
                         .map { $0.0 + [$0.1] }
                         .eraseToAnyPublisher()
                 }
         }
-        .flatMap { (pages: [LifeWikiPatternPage]) -> AnyPublisher<[LifeWikiRLE?], Never> in
+        .flatMap { (pages: [LifeWikiPatternPage]) -> AnyPublisher<[(LifeWikiPatternPage, LifeWikiRLE?)], Never> in
             print("⚡️ Start fetch RLE.")
+            let initial = Just([LifeWikiRLE?]()).eraseToAnyPublisher()
             return pages
-                .reduce(
-                    Just([LifeWikiRLE?]()).eraseToAnyPublisher()
-                ) { (result, page) in
-                    if let url = page.rleURL {
-                        return result.zip(LifeWikiRLE.fetch(url: url))
-                            .map {
-                                $0.0 + [$0.1]
-                            }
-                            .eraseToAnyPublisher()
-                    } else {
-                        print("❌ RLE is not found (\(page)") // TODO: source URL を出力するように
+                .reduce(initial) { (result, page) in
+                    guard let url = page.rleURL else {
+                        print("❌ RLE is not found (\(page.sourceURL)")
                         return result
                     }
+                    return result.zip(LifeWikiRLE.fetch(url: url))
+                        .map { $0.0 + [$0.1] }
+                        .eraseToAnyPublisher()
                 }
+                .map { Array(zip(pages, $0)) }
+                .eraseToAnyPublisher()
+            
         }
-        .sink { rles in
-            print("⭐ Found \(rles.count) pages.")
-            print(rles)
+        .sink { results in
+            print("⭐ Found \(results.count) pages.")
+            let page = results.first!.0
+            let rle = results.first!.1
+            print("📄 \(page)")
+            print("🔖 \(rle!)")
             exit(0)
         }
 }
